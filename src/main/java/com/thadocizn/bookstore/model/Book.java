54e0654fa519aafa1b410778ccc6909dc8c4f4c1
ThaDocIzN <rodan72@msn.com>
2019-03-01 11:19:41 -0600

    added book class

---
+++ src/main/java/com/thadocizn/bookstore/model/Book.java
@@ -0,0 +1,51 @@
+package com.thadocizn.bookstore.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookid;
+
+    private String booktitle;
+    private String ISBN;
+    private int copy;
+
+    public Book() {
+    }
+
+    public long getBookid() {
+        return bookid;
+    }
+
+    public void setBookid(long bookid) {
+        this.bookid = bookid;
+    }
+
+    public String getBooktitle() {
+        return booktitle;
+    }
+
+    public void setBooktitle(String booktitle) {
+        this.booktitle = booktitle;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public int getCopy() {
+        return copy;
+    }
+
+    public void setCopy(int copy) {
+        this.copy = copy;
+    }
+}
